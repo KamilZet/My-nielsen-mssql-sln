06b4c9ec95c3803baeca5739634a1a8e1307e68e
Kamil Zień <kamilxzien@gmail.com>
2016-06-27 22:54:50 +0200

    wymagania procesu

--- LearnExamples/YYYYYYYYY.sql
+++ LearnExamples/YYYYYYYYY.sql
@@ -1,54 +1,53 @@
-declare @changedLinks table (plnid int,storeid int ,storename nvarchar(max))
 
-INSERT @changedLinks VALUES (
-		1
-		,1
-		,'a'
-	),(
-		1
-		,2
-		,'b'
-	),(
-		2
-		,1
-		,'zz'
-	),(
-		3
-		,NULL
-		,NULL
-	)
+--current
+declare @storePlnMap table (sysplnid int,syssid int)
+--change
+declare @inserted table (sysplnid int,syssid int)
+declare @refStoreDict table (syssid int,sid int)
+declare @refPlnDict table(sysplnid int,plnid int)
+
+
+-- rodzaje asocjacji:
+	--przypisane wszystkie sklepy -> po stronie sys {pe³na lista || wpis 'general} -> po stronie pln wpis 'general'
+	--przypisane niewszystkie sklepy -> po stronie sys pe³na lista -> po stronie pln wpis'multiple'
+	--przypisany dok³adnie jeden sklep -> po stronie sys jeden wpis -> po stronie pln wpis <dane sklepu z sys>
+	--brak sklepów -> po stronie sys NULL -> po stronie pln wpis <no store>
+
+-- oddzielna procedury aktualizacji, dodania, usuniêcia
+
+insert @storePlnMap values (1,1),(1,2),(3,1)
+
+INSERT @inserted
+	VALUES (1),(2),(3)
 
 SELECT
 	*
-FROM
-	@changedLinks
+FROM @changedLinks
 
 ;
-WITH
-	gr
-	AS (SELECT
-			plnid
-			,COUNT(storeid) AS linkedStoreCount
-		FROM @changedLinks
-		GROUP BY plnid)
+WITH gr
+AS (SELECT
+		plnid
+		,COUNT(storeid) AS linkedStoreCount
+	FROM @changedLinks
+	GROUP BY plnid)
 
 SELECT
 	plnid
 	,CASE
 		WHEN linkedStoreCount = 0 THEN '-1'
 		WHEN linkedStoreCount = 1 THEN (SELECT TOP 1
-				storeid
-			FROM @changedlinks
-			WHERE plnid = a.plnid)
+					storeid
+				FROM @changedlinks
+				WHERE plnid = a.plnid)
 		WHEN linkedStoreCount > 1 THEN '-2'
 	END
 	,CASE
 		WHEN linkedStoreCount = 0 THEN 'general'
 		WHEN linkedStoreCount = 1 THEN (SELECT TOP 1
-				storename
-			FROM @changedlinks
-			WHERE plnid = a.plnid)
+					storename
+				FROM @changedlinks
+				WHERE plnid = a.plnid)
 		WHEN linkedStoreCount > 1 THEN 'multiple'
 	END
-FROM
-	gr a
+FROM gr a
